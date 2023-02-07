a37b78b63fc54eafe828c2ffbd9c66d2082be467
Esther Raphael <estherraphael126@gmail.com>
2023-02-07 20:28:35 +0100

    myComposablesUpdated

---
+++ src/composables/colorPicks.js
@@ -0,0 +1,25 @@
+import { ref } from "vue";
+const colorPicks = () => {
+    const colors = ["green", "red", "blue", "purple","yellow"];
+
+    let message = ref("Pick a color...");
+
+    const matchColor = (value) => {
+        // do a random color based on the array index;
+        const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+
+        if (colors[randomNumber] === value) {
+          message.value = `You win... [answer: ${colors[randomNumber]}]`;
+          return;
+        }
+
+        message.value = `You loose [answer: ${colors[randomNumber]}]`;
+      }
+    return{
+      colors,
+      message,
+      matchColor
+    };
+  };
+
+  export default colorPicks;
